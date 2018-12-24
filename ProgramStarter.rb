1c68191eb999fe2ef541a4ca8aa5103efbd86b46
Lucas Combs <l-combs@hotmail.com>
2018-12-24 10:34:44 -0500

    Now reading task list from JSON

--- ProgramStarter.rb
+++ ProgramStarter.rb
@@ -1,25 +1,30 @@
 #!/usr/bin/ruby
 
 require 'set'
+require_relative 'Task'
+require 'fileutils'
 
-$PROGRAM_LIST = [
-	["Calculator", "calc.exe"],
-	["Notes", "notepad.exe"],
-]
+$PROGRAM_LIST = []
+$TASK_FILE = "tasks.JSON"
 
 # Make system call to generate set of open programs
 def getOpenPrograms()
 	programs = Set.new()
 		(`TASKLIST /FO CSV`).split("\n").each do |line|
-			# Put lowercase program name in set
+			# Extract lowercase program name and add to set
 			programs.add(line[1,line.index("\"", 1)-1].downcase())
 		end
 	return programs
 end
 
+file = File.new($TASK_FILE)
+JSON.parse(file.read).each do |task|
+	$PROGRAM_LIST.push(Task.fromJSON(task))
+end
+
 # Open each program in array
 $PROGRAM_LIST.each do |program|
-	system("START \"#{program[0]}\" #{program[1]}")
+	program.start()
 end
 
 # Print set of running programs
