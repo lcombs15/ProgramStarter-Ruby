102a7d6c591f0979f67ba394ccddc1d8667f6d4a
Lucas Combs <l-combs@hotmail.com>
2018-12-25 23:52:55 -0500

    Some cleanup - added report and halt time

--- ProgramStarter.rb
+++ ProgramStarter.rb
@@ -3,14 +3,13 @@
 require 'set'
 require_relative 'Task'
 require 'fileutils'
-require_relative 'JSON_Manager'
 
-$PROGRAM_LIST = []
 $TASK_FILE = "tasks.JSON"
 
 # Make system call to generate set of open programs
 def getOpenPrograms()
 	programs = Set.new()
+		# Loop through CSV formatted list of tasks
 		(`TASKLIST /FO CSV`).split("\n").each do |line|
 			# Extract lowercase program name and add to set
 			programs.add(line[1,line.index("\"", 1)-1].downcase())
@@ -18,28 +17,35 @@ def getOpenPrograms()
 	return programs
 end
 
-JSON.parse(File.new($TASK_FILE).read)["Tasks"].each do |task|
-	$PROGRAM_LIST.push(Task.fromJSON(task))
+# Read tasks from JSON file
+def getTasksFromJSON()
+	retVal = []
+	JSON.parse(File.new($TASK_FILE).read)["Tasks"].each do |task|
+		retVal.push(Task.fromJSON(task))
+	end
+	return retVal
 end
+
+# Output heading
+puts "Task\t\tStatus"
+puts "-" * 50
 
 openPrograms = getOpenPrograms()
 
 # Open each program in array
-$PROGRAM_LIST.each do |program|
+getTasksFromJSON().each do |program|
+	print "#{program.name}:\t"
 	if !openPrograms.include?(program.exe.downcase()) then
 		program.start()
-		puts "Started: #{program.name}"
+		print "Started"
 	else
-		puts "Skipped #{program.name}, already running"
+		print "***Already Running***"
 	end
+	
+	# New line
+	puts
 end
 
-tasks = []
-
-$PROGRAM_LIST.each do |task|
-	tasks.push(task.toHash())
-end
-
-output = Hash.new()
-output["Tasks"] = tasks
-writeJSONFile("out.JSON", output)
+sleep_time = 5
+puts "Complete. Halting for #{sleep_time} second(s)..."
+sleep(sleep_time)
